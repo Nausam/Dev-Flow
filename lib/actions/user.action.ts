a18ae74b06c2febce3221dd47fbc087dea17f2f1
Nausam <nausam.hussain@gmail.com>
2023-10-16 05:10:20 +0500

    Fixed some typscript errors

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -71,9 +71,9 @@ export async function deleteUser(params: DeleteUserParams) {
     // and questions, answers, comments, etc.
 
     // Get user's question ids
-    const userQuestionIds = await Question.find({ author: user._id }).distinct(
-      "_id"
-    );
+    // const userQuestionIds = await Question.find({ author: user._id }).distinct(
+    //   "_id"
+    // );
 
     // Delete user's questions
     await Question.deleteMany({ author: user._id });
